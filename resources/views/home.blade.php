1927c229e7409694ac646c9cacf2167a77813761
fdcoull <fdcoull@users.noreply.github.com>
2023-07-13 00:55:53 +0100

    Add post functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,20 @@
 <body>
 
     @auth
-    <h3>Login successful.</h3>
+    <h3>You are logged in.</h3>
     <form action="/logout" method="POST">
         @csrf
         <button>Log out</button>
     </form>
+    <div style="border: 3px solid black">
+        <h3>Create new post</h3>
+        <form action="/create-post" method="POST">
+            @csrf
+            <input type="text" name="title" placeholder="Title">
+            <textarea placeholder="Type post here..." name="body"></textarea>
+            <button>Post</button>
+        </form>
+    </div>
     @else
     <div style="border: 3px solid black">
         <h3>Sign up</h3>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -18,6 +19,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+//User routes
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/logout', [UserController::class, 'logout']);
 Route::post('/login', [UserController::class, 'login']);
+
+//Post routes
+Route::post('/create-post', [PostController::class, 'createPost']);
